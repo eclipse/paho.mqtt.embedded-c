ae4deaed01ba05b284449982c578028e4ddd4e33
Ian Craggs <icraggs@uk.ibm.com>
2014-08-25 13:20:16 +0100

    Add linux samples for embedded C++ client

---
+++ MQTTClient/src/linux/build.sh
@@ -0,0 +1,3 @@
+g++ hello.cpp -I .. -I ../../../MQTTPacket/src ../../../MQTTPacket/src/MQTTPacket.c ../../../MQTTPacket/src/MQTTDeserializePublish.c ../../../MQTTPacket/src/MQTTConnectClient.c ../../../MQTTPacket/src/MQTTSubscribeClient.c ../../../MQTTPacket/src/MQTTSerializePublish.c ../../../MQTTPacket/src/MQTTUnsubscribeClient.c -o hello
+
+g++ stdoutsub.cpp -I .. -I ../../../MQTTPacket/src ../../../MQTTPacket/src/MQTTPacket.c ../../../MQTTPacket/src/MQTTDeserializePublish.c ../../../MQTTPacket/src/MQTTConnectClient.c ../../../MQTTPacket/src/MQTTSubscribeClient.c ../../../MQTTPacket/src/MQTTSerializePublish.c -o stdoutsub
