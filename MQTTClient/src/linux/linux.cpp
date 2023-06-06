8da9c671e87108a837a06bea8d241e6c71c69170
Ian Craggs <icraggs@gmail.com>
2023-06-06 16:43:38 +0100

    Ignore SIGPIPE on Linux

--- MQTTClient/src/linux/linux.cpp
+++ MQTTClient/src/linux/linux.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2017 IBM Corp.
+ * Copyright (c) 2014, 2023 IBM Corp.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -39,7 +39,7 @@ class IPStack
 public:
   IPStack()
   {
-
+		signal(SIGPIPE, SIG_IGN);
   }
 
   int connect(const char* hostname, int port)
